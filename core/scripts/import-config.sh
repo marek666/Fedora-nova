2ec46ea59197e78aa13839aaf4bd94af436426fc
marek666 <marek.suranic@gmail.com>
2026-09-11 23:24:38 +0200

    fix(import): treat empty dconf sections as no-op

--- core/scripts/import-config.sh
+++ core/scripts/import-config.sh
@@ -36,8 +36,9 @@ PAYLOAD="$TMP/fedora-nova-export"
 
 load() {
   local file="$1" path="$2"
-  [[ -s "$PAYLOAD/dconf/$file.dconf" ]] &&
+  if [[ -s "$PAYLOAD/dconf/$file.dconf" ]]; then
     dconf load "$path" <"$PAYLOAD/dconf/$file.dconf"
+  fi
 }
 load interface /org/gnome/desktop/interface/
 load wm-preferences /org/gnome/desktop/wm/preferences/
